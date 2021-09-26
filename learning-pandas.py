80dcd74c953a91e5dc92c7eaa7aad27b8af217c7
Dmytro Novik <exfriend@live.ru>
2021-09-26 17:58:46 +0300

    iloc

--- learning-pandas.py
+++ learning-pandas.py
@@ -3,4 +3,6 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
 
 
-print(df.loc[[5, 10, 15], ['Name', 'Age']].tail(1))
+#print(df.loc[[5, 10, 15], ['Name', 'Age']].tail(1))
+
+print(df.iloc[[5, 10, 15], [0, 9]])
