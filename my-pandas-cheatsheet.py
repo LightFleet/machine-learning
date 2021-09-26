30cfff57155e10f27d44ba35bee5c4b5c3bb896d
Dmytro Novik <exfriend@live.ru>
2021-09-26 18:51:02 +0300

    analytical functions

--- my-pandas-cheatsheet.py
+++ my-pandas-cheatsheet.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
 
-
+# --- БАЗОВЫЕ ОПЕРАЦИЕ ПО ВЫБОРКЕ ---
 # print(df.loc[[5, 10, 15], ['Name', 'Age']].tail(1))
 
 # Нулевая и девятая колонка элементов с индексами 5, 10 и 15
@@ -20,20 +20,40 @@ df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/mas
 # Возвращает маску (карту) где проверяет есть ли элемент на этом месте
 # print(df['Age'].notna())
 
-# Ну типа наоборот, но тут ещё и пропуск
+# Ну типа наоборот, но тут ещё и сумма получаецца
 # Тоесть тут кол-во пассажиров Титаника возраст которых неизвестен
 # print(df['Age'].isna().sum())
 
 # Имена всех людей для которых известен возраст
 # print(df.loc[df['Age'].notna(), 'Name'])
 
-### СОРТИРОВКА
+# --- СОРТИРОВКА ---
 
 # Первые 10 записей отсортированные по возрасту
 # print(df.sort_values('Age').head(10))
 
 # Первые 10 записей отсортированные по убыванию возраста и по имени в алфавитном порядке
-print(df.sort_values(['Age', 'Name'], ascending=[False, True]).head(10))
+# print(df.sort_values(['Age', 'Name'], ascending=[False, True]).head(10))
 
+# -- Копируем --
+# df2 = df.copy(deep=True)
 
+# Присобачиваем один датафрейм под низ другого датафрейма
+# cdf1 = pd.concat([df, df2])
 
+# --- АНАЛИТИЧЕСКИЕ ФУНКЦИИ ---
+
+# Кол-во не пустых элементов по всем колонкам
+# print(df.count())
+
+# Кол-во элм-ов по колоночке
+# print(df['Age'].count())
+
+# -- СРЕДНЕЕ И МЕДИАНА --
+# print(df['Age'].mean(), df['Age'].median())
+
+# -- НЕМНОГО ГОТОВОЙ СТАТИСТИКИ (ура) (среднее/мин/макс значения, медиана, квантили) --
+# print(df['Age'].describe())
+
+# -- Группируем --
+# in next releases (49.99$)
