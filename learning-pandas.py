b460d4262e2a492bb4ea9a2dc3dd8ee58cf7a184
Dmytro Novik <exfriend@live.ru>
2021-09-26 18:05:45 +0300

    series type object

--- learning-pandas.py
+++ learning-pandas.py
@@ -3,8 +3,14 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv')
 
 
-#print(df.loc[[5, 10, 15], ['Name', 'Age']].tail(1))
+# print(df.loc[[5, 10, 15], ['Name', 'Age']].tail(1))
 
-#print(df.iloc[[5, 10, 15], [0, 9]])
+# Нулевая и девятая колонка элементов с индексами 5, 10 и 15
+# print(df.iloc[[5, 10, 15], [0, 9]])
 
-print(df.iloc[5:24, :3])
+# Первые три колонки элементов с индексом от 5 до 24
+# print(df.iloc[5:24, :3])
+
+# df['Age'] > 18 - объект типа Series (true/false проставил) который передали в df и получили выборку по маске!
+# print(df[df['Age'] > 18])
+
