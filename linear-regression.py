739cade026b2ea3dffbd480bfed30bcee46ed293
Dmytro Novik <exfriend@live.ru>
2021-10-03 20:43:41 +0300

    setting up a quandl dataset

--- linear-regression.py
+++ linear-regression.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import matplotlib
+import quandl
+import config
+
+quandl.ApiConfig.api_key = config.quand_api_key
+
+df = quandl.get('WIKI/GOOGL')
+
+# df.plot(kind='as', legend=True)
+# matplotlib.pyplot.show()
+
+print(df)
